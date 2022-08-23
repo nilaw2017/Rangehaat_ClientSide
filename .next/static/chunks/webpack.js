439f2c29d4565a345bc9f4f069a3ff015207daca
nilaw2017 <nilaw.manandhar2016@gmail.com>
2022-08-23 13:40:40 +0545

    UI v.4 textJustify

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ac430d35451491f8"; }
+/******/ 		__webpack_require__.h = function() { return "dccdfbd50e22e112"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
